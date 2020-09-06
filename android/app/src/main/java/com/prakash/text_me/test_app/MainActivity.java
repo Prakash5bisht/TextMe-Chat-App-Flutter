f5df7e795994db12ea12aecf266bb04319037a34
Prakash5bisht <singhbprakash505@gmail.com>
2020-09-06 06:51:52 -0700

    working on splash screen

--- android/app/src/main/java/com/prakash/text_me/test_app/MainActivity.java
+++ android/app/src/main/java/com/prakash/text_me/test_app/MainActivity.java
@@ -5,9 +5,14 @@ import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+import android.view.WindowManager;
+//import android.view.ViewTreeObserver;
+
 public class MainActivity extends FlutterActivity {
+
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
     GeneratedPluginRegistrant.registerWith(flutterEngine);
+     getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
   }
 }
