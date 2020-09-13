331372c518e65ec501fa9e2a1b6eb7993ba85907
Prakash5bisht <singhbprakash505@gmail.com>
2020-09-13 00:25:29 -0700

    changed customAppBar style

--- lib/screens/country_picker_screen.dart
+++ lib/screens/country_picker_screen.dart
@@ -39,86 +39,166 @@ class _CountryPickerState extends State<CountryPicker> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: customAppBar(context),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: ListView.builder(
-                itemCount: country.countries.length,
-                itemBuilder: (BuildContext context, int index) {
-                  // i have added toLowercase() to avoid case sensitive search
-                  return filter == null || filter == '' ? new CustomTiles(country.countries[index]["name"], country.countries[index]["code"])
-                      : (country.countries[index]["name"].toLowerCase().contains((filter).toLowerCase())
-                          ? CustomTiles(country.countries[index]["name"],
-                              country.countries[index]["code"])
-                          : Container());
-                },
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Colors.white,
+         // appBar: customAppBar(context),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 6.0, left: 4.0, right: 4.0, bottom: 4.0),
+                child: Container(
+                  width: 100.0,
+                  height: 50.0,
+                 child: customAppBar(context),//customAppBar(context),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(width: 0.3, color: Color(0xffd9d9d9)),
+                      borderRadius: BorderRadius.circular(25.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0x10263238),
+                          blurRadius: 10.0,
+                         // offset: Offset(0, 5)
+                        )
+                      ]
+                  ),
+                ),
               ),
-            )
-          ],
+              Expanded(
+                child: Scrollbar(
+                  child: ListView.builder(
+                    itemCount: country.countries.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      // i have added toLowercase() to avoid case sensitive search
+                      return filter == null || filter == '' ? new CustomTiles(country.countries[index]["name"], country.countries[index]["code"])
+                          : (country.countries[index]["name"].toLowerCase().contains((filter).toLowerCase())
+                              ? CustomTiles(country.countries[index]["name"],
+                                  country.countries[index]["code"])
+                              : Container());
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
-      );
-  }
-
-  Widget customAppBar(BuildContext context) {
-    return AppBar(
-      backgroundColor: Color(0xff262626),
-      elevation: 0.0,
-      centerTitle: true,
-      leading: IconButton(
-        icon: Icon(
-          Icons.keyboard_arrow_left,
-          color: Colors.white,
-        ),
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      ),
-      title: appBarTitle,
-      actions: <Widget>[
-        IconButton(
-            icon: Icon(
-              icon,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              if (icon == Icons.search) {
-                setState(() {
-                  this.appBarTitle = TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Search countries',
-                      hintStyle: kAppBarTextFieldHintStyle,
-                      border: InputBorder.none,
-                    ),
-                    controller: controller,
-                    style: kAppBarTextFieldStyle,
-                    cursorColor: Colors.grey[500],
-                    autofocus: true,
-                  );
-                  icon = Icons.clear;
-                });
-              } else {
-                setState(() {
-                  icon = Icons.search;
-                  appBarTitle = Text(
-                    'Search Country',
-                    style: kAppBarTextStyle,
-                  );
-                  controller.clear();
-                });
-              }
-            }),
-      ],
-     // elevation: 2.0,
-      // shape: RoundedRectangleBorder(
-      //   borderRadius: BorderRadius.only(
-      //     bottomLeft: Radius.circular(20.0), bottomRight: Radius.circular(20.0),
-      //   ),
-      // ),
     );
   }
+
+  // Widget customAppBar(BuildContext context) {
+  //   return AppBar(
+  //     backgroundColor: Color(0xff262626),
+  //     elevation: 0.0,
+  //     centerTitle: true,
+  //     leading: IconButton(
+  //       icon: Icon(
+  //         Icons.keyboard_arrow_left,
+  //         color: Colors.white,
+  //       ),
+  //       onPressed: () {
+  //         Navigator.pop(context);
+  //       },
+  //     ),
+  //     title: appBarTitle,
+  //     actions: <Widget>[
+  //       IconButton(
+  //           icon: Icon(
+  //             icon,
+  //             color: Colors.white,
+  //           ),
+  //           onPressed: () {
+  //             if (icon == Icons.search) {
+  //               setState(() {
+  //                 this.appBarTitle = TextField(
+  //                   decoration: InputDecoration(
+  //                     hintText: 'Search countries',
+  //                     hintStyle: kAppBarTextFieldHintStyle,
+  //                     border: InputBorder.none,
+  //                   ),
+  //                   controller: controller,
+  //                   style: kAppBarTextFieldStyle,
+  //                   cursorColor: Colors.grey[500],
+  //                   autofocus: true,
+  //                 );
+  //                 icon = Icons.clear;
+  //               });
+  //             } else {
+  //               setState(() {
+  //                 icon = Icons.search;
+  //                 appBarTitle = Text(
+  //                   'Search Country',
+  //                   style: kAppBarTextStyle,
+  //                 );
+  //                 controller.clear();
+  //               });
+  //             }
+  //           }),
+  //     ],
+  //    // elevation: 2.0,
+  //     // shape: RoundedRectangleBorder(
+  //     //   borderRadius: BorderRadius.only(
+  //     //     bottomLeft: Radius.circular(20.0), bottomRight: Radius.circular(20.0),
+  //     //   ),
+  //     // ),
+  //   );
+  // }
+
+  Widget customAppBar(BuildContext context){
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.keyboard_arrow_left,
+              color: Colors.black45,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+         Flexible(
+           child: appBarTitle,
+         ),
+          IconButton(
+              icon: Icon(
+                icon,
+                color: Colors.black45,
+              ),
+              onPressed: () {
+                if (icon == Icons.search) {
+                  setState(() {
+                    this.appBarTitle = TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search countries',
+                        hintStyle: kAppBarTextFieldHintStyle,
+                        border: InputBorder.none,
+                      ),
+                      controller: controller,
+                      style: kAppBarTextFieldStyle,
+                      cursorColor: Colors.black26,
+                      autofocus: true,
+                    );
+                    icon = Icons.clear;
+                  });
+                } else {
+                  setState(() {
+                    icon = Icons.search;
+                    appBarTitle = Text(
+                      'Search Country',
+                      style: kAppBarTextStyle,
+                    );
+                    controller.clear();
+                  });
+                }
+              }),
+        ],
+      ),
+    );
+  }
+
 }
 
 class CustomTiles extends StatelessWidget {
