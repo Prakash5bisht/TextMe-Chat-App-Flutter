9f07bb41dda24f12717431032f9b4fba16be57ff
Prakash5bisht <singhbprakash505@gmail.com>
2020-09-14 02:33:43 -0700

    created OTP verification_screen

---
+++ lib/screens/verification_screen.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:test_app/components/custom_alert_dialog.dart';
+
+class VerificationScreen extends StatefulWidget {
+  static const String id = 'verification_screen';
+  @override
+  _VerificationScreenState createState() => _VerificationScreenState();
+}
+
+class _VerificationScreenState extends State<VerificationScreen> {
+  Timer _timer;
+  int _start = 30;
+
+  @override
+  void initState() {
+   // otpValidationPeriod();
+    super.initState();
+  }
+  void otpValidationPeriod(){
+    _start = 30;
+    const oneSecond = const Duration(seconds: 1);
+    _timer = Timer.periodic(
+        oneSecond,
+            (timer) =>  setState(() {
+              if(_start < 1){
+                _start = 00;
+                timer.cancel();
+              }
+              else{
+                _start = _start - 1;
+              }
+            }) );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topLeft,
+                child: IconButton(
+                  icon: Icon(Icons.chevron_left, size: 28.0,),
+                  color: Color(0xff263238),
+                  onPressed: (){
+
+                  },
+                ),
+              ),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text('Verification', style: TextStyle(color: Color(0xff9b9aac), fontWeight: FontWeight.w700),),
+                      SizedBox(height: 8.0,),
+                      Flexible(
+                        child: Text(
+                          'We sent you an SMS code',
+                          style: TextStyle(
+                            color: Color(0xff383852),
+                            fontWeight: FontWeight.w900,
+                            fontSize: 24.0,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 6.0,),
+                      Row(
+                        children: <Widget>[
+                          Text('Phone Number:', style: TextStyle(color: Color(0xff9b9aac), fontWeight: FontWeight.w700),),
+                          SizedBox(width: 5.0,),
+                          Text('+91 123456789', style: TextStyle(color: Color(0xff393ac5), fontWeight: FontWeight.w500),),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Flexible(
+                 child: Column(
+                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                   children: <Widget>[
+                    Container(
+                      height: size.height/10.0,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(width: 0.3, color: Color(0xffd9d9d9)),
+                          borderRadius: BorderRadius.circular(12.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0x10263238),
+                              blurRadius: 10.0,
+                              // offset: Offset(0, 5)
+                            )
+                          ]
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none
+                        ),
+                      ),
+                    ),
+                     Text(
+                        _start>9 ? '00:$_start' : '00:0$_start',
+                       style: TextStyle(color: Colors.black38, fontWeight: FontWeight.w500),
+                     )
+                   ],
+                 ),
+              ),
+              Flexible(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    MaterialButton(
+                      height: 45.0,
+                      elevation: 5.0,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                      color: Color(0xff263238),
+                      child: Text('Verify', style: TextStyle(color: Colors.white),),
+                      onPressed: (){
+                       showAlert(context, 'Error', 'The OTP you entered was incorrect');
+                      },
+                    ),
+                    SizedBox(height: 6.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('Didn\'t received code?', style: TextStyle(color: Color(0xff9b9aac), fontWeight: FontWeight.w500),),
+                        FlatButton(
+                          child: Text('Resend' , style: TextStyle(color: _start >= 1 ? Colors.grey : Color(0xff393ac5), fontWeight: FontWeight.w500),),
+                          onPressed: _start > 1 ? null : otpValidationPeriod,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
