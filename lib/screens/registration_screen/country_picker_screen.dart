454066fee0b74032a40c2ab0786d3fd7e6272b41
Prakash5bisht <singhbprakash505@gmail.com>
2020-09-18 00:31:37 -0700

    made some style changes

--- lib/screens/registration_screen/country_picker_screen.dart
+++ lib/screens/registration_screen/country_picker_screen.dart
@@ -5,7 +5,7 @@ import 'package:test_app/constants.dart';
 
 Country country = Country();
 
-// void main() => runApp(CountryPicker());
+ void main() => runApp(CountryPicker());
 
 class CountryPicker extends StatefulWidget {
   static const String id = 'country_picker_screen';
@@ -18,7 +18,7 @@ class _CountryPickerState extends State<CountryPicker> {
   String filter;
   IconData icon = Icons.search;
   Widget appBarTitle = Text(
-    'Search Country',
+    '',
     style: kAppBarTextStyle,
   );
   @override
@@ -39,51 +39,76 @@ class _CountryPickerState extends State<CountryPicker> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-          backgroundColor: Colors.white,
-         // appBar: customAppBar(context),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 6.0, left: 4.0, right: 4.0, bottom: 4.0),
-                child: Container(
-                  width: 100.0,
-                  height: 50.0,
-                 child: customAppBar(context),//customAppBar(context),
-                  decoration: BoxDecoration(
-                      color: Color(0xff263238),
-                      border: Border.all(width: 0.3, color: Color(0xffd9d9d9)),
-                      borderRadius: BorderRadius.circular(25.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color(0x10263238),
-                          blurRadius: 10.0,
-                         // offset: Offset(0, 5)
-                        )
-                      ]
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+            backgroundColor: Colors.white,
+           // appBar: customAppBar(context),
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: Row(
+                    children: <Widget>[
+                       IconButton(
+                          icon: Icon(Icons.chevron_left, color: Color(0xff666666), size: 30.0,),
+                          onPressed: (){
+
+                          },
+                        ),
+                      Text(
+                        'Select your country',
+                        style: TextStyle(
+                          color: Color(0xff4d4d4d),
+                          fontWeight: FontWeight.w900,
+                          fontSize: 20.0,
+                          fontFamily: 'Poppins'
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              Expanded(
-                child: Scrollbar(
-                  child: ListView.builder(
-                    itemCount: country.countries.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      // i have added toLowercase() to avoid case sensitive search
-                      return filter == null || filter == '' ? new CustomTiles(country.countries[index]["name"], country.countries[index]["code"])
-                          : (country.countries[index]["name"].toLowerCase().contains((filter).toLowerCase())
-                              ? CustomTiles(country.countries[index]["name"],
-                                  country.countries[index]["code"])
-                              : Container());
-                    },
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                  child: Container(
+                    width: 100.0,
+                    height: 50.0,
+                   child: customAppBar(context),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                       // border: Border.all(width: 1.8, color: Color(0xfff2f5f8)),
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0xffDCDCE5),
+                           blurRadius: 3.0,
+                          )
+                        ]
+                    ),
                   ),
                 ),
-              )
-            ],
+                SizedBox(height: 2.0,),
+                Expanded(
+                  child: Scrollbar(
+                    child: ListView.builder(
+                      itemCount: country.countries.length,
+                      physics: BouncingScrollPhysics(),
+                      itemBuilder: (BuildContext context, int index) {
+                        // i have added toLowercase() to avoid case sensitive search
+                        return filter == null || filter == '' ? new CustomTiles(country.countries[index]["name"], country.countries[index]["code"])
+                            : (country.countries[index]["name"].toLowerCase().contains((filter).toLowerCase())
+                                ? CustomTiles(country.countries[index]["name"],
+                                    country.countries[index]["code"])
+                                : Container());
+                      },
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
+      ),
     );
   }
 
@@ -151,21 +176,9 @@ class _CountryPickerState extends State<CountryPicker> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           IconButton(
-            icon: Icon(
-              Icons.keyboard_arrow_left,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-         Flexible(
-           child: appBarTitle,
-         ),
-          IconButton(
               icon: Icon(
                 icon,
-                color: Colors.white,
+                color: Color(0xff4c4b56),
               ),
               onPressed: () {
                 if (icon == Icons.search) {
@@ -178,7 +191,7 @@ class _CountryPickerState extends State<CountryPicker> {
                       ),
                       controller: controller,
                       style: kAppBarTextFieldStyle,
-                      cursorColor: Colors.white,
+                      cursorColor: Color(0xff4d4d4d),
                       autofocus: true,
                     );
                     icon = Icons.clear;
@@ -187,13 +200,16 @@ class _CountryPickerState extends State<CountryPicker> {
                   setState(() {
                     icon = Icons.search;
                     appBarTitle = Text(
-                      'Search Country',
+                      '',
                       style: kAppBarTextStyle,
                     );
                     controller.clear();
                   });
                 }
               }),
+         Flexible(
+           child: appBarTitle,
+         ),
         ],
       ),
     );
@@ -209,56 +225,67 @@ class CustomTiles extends StatelessWidget {
   Widget build(BuildContext context) {
     var size  = MediaQuery.of(context).size;
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 6.0),
+      padding: const EdgeInsets.symmetric(horizontal: 22.0, vertical: 6.0),
       child: GestureDetector(
         onTap: () {
-          Navigator.of(context).pop([countryDialCode, countryName]);
+         // Navigator.of(context).pop([countryDialCode, countryName]);
+          print('tapped');
         },
         child: Container(
-          height: size.height/8.0,
+          height: size.height/13.0,
           width: size.width/3.0,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10.0),
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0xffDCDCE5),
-                  blurRadius: 10.0,
-                )
-              ]),
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Flexible(
-                  child: CircleAvatar(
-                    radius: size.height/33.0,
-                    backgroundImage: AssetImage('images/${countryName.toLowerCase()}.png'),
-                    backgroundColor: Colors.grey[100],
+          // decoration: BoxDecoration(
+          //     color: Colors.white, //Color(0xffdbe8fe)
+          //     borderRadius: BorderRadius.circular(10.0),
+          //     // boxShadow: [
+          //     //   BoxShadow(
+          //     //     color: Color(0xffDCDCE5),
+          //     //     blurRadius: 10.0,
+          //     //   )
+          //     // ],
+          // ),
+          child:
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Flexible(
+                    child: Material(
+                      shape: CircleBorder(),
+                      elevation: 4.0,
+                      shadowColor: Color(0xffDCDCE5),
+                      child: CircleAvatar(
+                        radius: size.height/40.0,
+                        backgroundImage: AssetImage('images/${countryName.toLowerCase()}.png'),//'images/${countryName.toLowerCase()}.png'
+                        backgroundColor: Colors.grey[100],
+                      ),
+                    ),
                   ),
-                ),
-                Flexible(
-                  child: Text(
+                  Text(
                     countryName,
+                    softWrap: false,
+                    textAlign: TextAlign.left,
                     style: TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xff404040)),
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Poppins',
+                        color: Color(0x900d0e13)//Color(0xff989dac)
+                    ),
                   ),
-                ),
-                Flexible(
-                  child: Text(
-                    '(+$countryDialCode)',
-                    style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xff666666)),
+                  Flexible(
+                    child: Text(
+                      '(+$countryDialCode)',
+                      style: TextStyle(
+                          fontSize: 13.5,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: 'Poppins',
+                          color: Color(0xff666666)),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
         ),
       ),
     );
