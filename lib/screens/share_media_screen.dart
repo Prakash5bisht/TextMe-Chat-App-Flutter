0745cdf530ca1e687f7455e22180ad4ac7025ee2
Prakash5bisht <singhbprakash505@gmail.com>
2020-09-19 08:35:35 -0700

    used custom icons instead of default ones

--- lib/screens/share_media_screen.dart
+++ lib/screens/share_media_screen.dart
@@ -1,11 +1,24 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:test_app/constants.dart';
+import 'package:test_app/custom_icons_set_icons.dart';
+import 'package:test_app/screens/chat_screen_components.dart';
+//import 'package:test_app/models/custom_appbar.dart';
 import 'package:test_app/screens/media_preview_screen.dart';
-import 'package:test_app/screens/testing.dart';
 
-class ShareMediaScreen extends StatelessWidget {
-  
+class ShareMediaScreen extends StatefulWidget {
+
+  @override
+  _ShareMediaScreenState createState() => _ShareMediaScreenState();
+}
+
+class _ShareMediaScreenState extends State<ShareMediaScreen> {
+
+  bool isPressed = false;
+  Color color = Color(0xff060607);
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +26,15 @@ class ShareMediaScreen extends StatelessWidget {
       padding: const EdgeInsets.only(top: 9.0),
       child: IconButton(
           icon: Icon(
-            Icons.share,
-            color: Color(0x99263238),
+            CustomIconsSet.share_social_interface_button,
+            color: Color(0xff309691),//Color(0x99263238),
             size: 30.0,
           ),
-          onPressed: () {
+          onPressed: () async{
+            // File file = await _statusFile;
+            // file.delete();
+            // print('done');
+           // Navigator.pushNamed(context, PhoneAuthentication.id);
             showModalBottomSheet(
               backgroundColor: Colors.transparent,
                 context: context,
@@ -38,17 +55,19 @@ class ShareMediaScreen extends StatelessWidget {
                         ]
                       ),
                       child: Padding(
-                        padding: const EdgeInsets.only(bottom: 25.0),
+                        padding: const EdgeInsets.only(bottom: 30.0, top: 18.0),
                         child: Row(
                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
                             Column(
                               children: <Widget>[
                                 Expanded(
-                                  child: IconButton(
-                                    icon: Icon(Icons.camera, size: 35.0, color: Colors.blueGrey,),
-                                    onPressed: (){
-                                      getMedia(context, ImageSource.camera);
+                                  child: ReusableButton(
+                                    highlightColor: Color(0xffd1cffc),
+                                    splashColor: Color(0xffd1cffc),
+                                    iconData: CustomIconsSet.photo_camera_outline,
+                                    iconColor: Colors.deepPurpleAccent,
+                                    function: (){// getMedia(context, ImageSource.camera);
                                     },
                                   ),
                                 ),
@@ -57,9 +76,10 @@ class ShareMediaScreen extends StatelessWidget {
                                   style: kShareMediaScreenTextStyle,
                                 ),
                                 Expanded(
-                                  child: IconButton(
-                                    icon: Icon(Icons.music_note, size: 35.0, color: Colors.deepOrangeAccent,),
-                                    onPressed: (){
+                                  child: ReusableButton(
+                                    iconData: CustomIconsSet.headphones_audio_symbol,//Colors.deepOrangeAccent
+                                    iconColor: Color(0xfffb9008),
+                                     function: (){
                                       print('ok');
                                     },
                                   ),
@@ -73,11 +93,12 @@ class ShareMediaScreen extends StatelessWidget {
                             Column(
                               children: <Widget>[
                                 Expanded(
-                                  child: IconButton(
-                                    icon: Icon(Icons.photo, size: 35.0, color: Colors.blue[600],),
-                                    onPressed: (){
+                                  child: ReusableButton(
+                                    iconData: CustomIconsSet.images_square_outlined_interface_button_symbol,//Colors.blue[600]
+                                    iconColor: Color(0xff309691),
+                                    function: (){
                                       try{
-                                        getMedia(context,ImageSource.gallery);
+                                       // getMedia(context,ImageSource.gallery);
                                       }catch(e){
                                         print(e);
                                       }
@@ -89,9 +110,10 @@ class ShareMediaScreen extends StatelessWidget {
                                     style: kShareMediaScreenTextStyle,
                                 ),
                                 Expanded(
-                                  child: IconButton(
-                                    icon: Icon(Icons.location_on, size: 35.0, color: Colors.red,),
-                                    onPressed: (){
+                                  child: ReusableButton(
+                                    iconData: CustomIconsSet.big_map_placeholder_outlined_symbol_of_interface,  //Colors.red
+                                    iconColor: Color(0xfffc2315),
+                                    function: (){
                                       print('ok');
                                     },
                                   ),
@@ -105,12 +127,13 @@ class ShareMediaScreen extends StatelessWidget {
                             Column(
                               children: <Widget>[
                                 Expanded(
-                                  child: IconButton(
-                                    icon: Icon(Icons.note_add, size: 35.0, color: Colors.deepPurpleAccent[100],),
-                                    onPressed: (){
-                                      ImagePicker().getVideo(source: ImageSource.gallery).then((video){
-                                        Navigator.push(context, MaterialPageRoute(builder: (context) => MediaPreviewScreen(media: video,) ));
-                                      });
+                                  child: ReusableButton(
+                                    iconData: CustomIconsSet.file_rounded_outlined_symbol, //Colors.deepPurpleAccent[100]
+                                    iconColor: Color(0xfffb9008),
+                                    function: (){
+                                      // ImagePicker().getVideo(source: ImageSource.gallery).then((video){
+                                      //   Navigator.push(context, MaterialPageRoute(builder: (context) => MediaPreviewScreen(media: video,) ));
+                                      // });
                                     },
                                   ),
                                 ),
@@ -119,9 +142,10 @@ class ShareMediaScreen extends StatelessWidget {
                                     style: kShareMediaScreenTextStyle,
                                 ),
                                 Expanded(
-                                  child: IconButton(
-                                    icon: Icon(Icons.call, size: 35.0, color: Colors.lightBlue,),
-                                    onPressed: (){
+                                  child: ReusableButton(
+                                    iconData: CustomIconsSet.phone_auricular_outline,  //Colors.lightBlue
+                                    iconColor: Colors.indigoAccent,
+                                    function: (){
 
                                     },
                                   ),
@@ -142,6 +166,17 @@ class ShareMediaScreen extends StatelessWidget {
           }
       ),
     );
+  }
+
+Future<String> get _path async{
+    final directory = await getApplicationDocumentsDirectory();
+    print(directory);
+    return directory.path;
+  }
+
+  Future<File> get _statusFile async{
+    final path = await  _path;
+    return File('$path/TextMe.txt');
   }
 
   void getMedia(context,mediaSource) async{
